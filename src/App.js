0e0f920198c11d5bfed12000634ad590c2df5881
Garrus Napp <garrusnapp@gmail.com>
2018-01-28 17:58:29 +0100

    sorting works, sort of

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class SearchForm extends Component {
@@ -27,13 +26,76 @@ class TableRow extends Component {
   }
 }
 class Results extends Component {
+  constructor() {
+    super()
+    this.state = {
+      sorting: "default"
+    }
+    this.handleClickSort = this.handleClickSort.bind(this)
+  }
+
+  handleClickSort() {
+    //on click toggle between 3 values
+    if (this.state.sorting === "default") {
+      this.setState({sorting: "ascending"})
+    }
+    else if (this.state.sorting === "ascending") {
+      this.setState({sorting: "descending"})
+    }
+    else if (this.state.sorting === "descending") {
+      this.setState({sorting: "default"})
+    }
+  }
+
   render() {
-    let rows = this.props.content.results.map((d, i) => <TableRow data={d} key={i}/>)
+    const sortSymbol = {
+      ascending: "Title ▲",
+      descending: "Title ▼",
+      default: "Title ▶"
+    }
+
+    let data = this.props.content.results
+
+    if (this.state.sorting === "default") {
+      //default is from most popular to least popular.
+      data.sort((a, b) => b.popularity - a.popularity)
+    }
+
+    if (this.state.sorting === "ascending") {
+      data.sort((a, b) => {
+        if (a.title < b.title) {
+          return -1
+        }
+        if (a.title > b.title) {
+          return 1
+        }
+        else {
+          return 0
+        }
+      })
+    }
+
+    if (this.state.sorting === "descending") {
+      data.sort((a, b) => {
+        if (a.title > b.title) {
+          return -1
+        }
+        if (a.title < b.title) {
+          return 1
+        }
+        else {
+          return 0
+        }
+      })
+    }
+
+    console.log(data[0])
+    let rows = data.map((d, i) => <TableRow data={d} key={i}/>)
     return (
       <table>
         <tbody>
         <tr>
-          <th>Poster</th><th>Title</th>
+          <th>Poster</th><th onClick={this.handleClickSort}>{sortSymbol[this.state.sorting]}</th>
           <th>Release date</th><th>Popularity</th>
           <th>Vote count</th><th>Vote avg.</th>
         </tr>
